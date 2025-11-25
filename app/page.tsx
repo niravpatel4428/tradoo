0c89d5deda93876cb07e89cd8b3518bd8146f5cb
hardik-gol <hardikgol.zindex@gmail.com>
2025-11-25 18:26:54 +0530

    banner with responsive done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Banner from "./sections/Banner";
+import Framework from "./sections/Framework";
 
 export default function Home() {
   return (
     <>
-      
+      <Banner/>
+      <Framework/>
     </>
   );
 }
