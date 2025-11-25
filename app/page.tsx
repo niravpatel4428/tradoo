c6b818496863f47a756333d1bbad98a65472ee06
dhavalzindex <dhaval.zindexsolutions@gmail.com>
2025-11-25 18:54:30 +0530

    page.tsx file section position change

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import Footer from "./sections/Footer";
-import WantPart from "./sections/WantPart";
-import Testimonials from "./sections/Testimonials";
 import Banner from "./sections/Banner";
 import Framework from "./sections/Framework";
+import WantPart from "./sections/WantPart";
+import Testimonials from "./sections/Testimonials";
+import Footer from "./sections/Footer";
 
 export default function Home() {
   return (
     <>
+      <Banner/>
+      <Framework/>
       <Testimonials/>
       <WantPart />
       <Footer />
-      <Banner/>
-      <Framework/>
     </>
   );
 }
